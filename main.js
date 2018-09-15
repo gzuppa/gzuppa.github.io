370ecdd11fd3f8e96e69025baab174c3888a1868
gzuppa <gustavo_zuppa_flores@hotmail.com>
2018-09-15 05:56:13 -0500

    fortis

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 swal("Los juegos de 99999 nunca existieron!", "...por eso solo tenemos tres :)");
+
+var cancion = new Audio();
+cancion.src = "./mortal-kombat-theme-song.mp3";
+
+function tocar(){
+cancion.play();
+}
+
+interval = setInterval(tocar);
+tocar();
