269b5241a28d6ff90cec712b5b3a9458c1b965d8
gzuppa <gustavo_zuppa_flores@hotmail.com>
2018-09-15 06:02:08 -0500

    sextis

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 swal("Los juegos de 99999 nunca existieron!", "...por eso solo tenemos tres :)");
 
-var cancion = new Audio();
-cancion.src = "./mortal-kombat-theme-song.mp3";
-
-function tocar(){
-cancion.play();
-}
-
-interval = setInterval(tocar);
-tocar();
