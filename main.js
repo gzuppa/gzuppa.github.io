80d22569ad3b4f4e6b71dfeddd66d48e21c1025e
gzuppa <gustavo_zuppa_flores@hotmail.com>
2018-09-15 06:46:30 -0500

    septis

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 swal("Los juegos de 99999 nunca existieron!", "...por eso solo tenemos tres :)");
 
+var cancion = new Audio();
+cancion.src = "./mk.mp3";
+
+function tocarCancion(){
+    cancion.play();
+}
+
+play.onclick = function start(){
+    setInterval (tocarCancion);
+}
+
